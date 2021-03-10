1a17ff8ca803a57ffe44bdd21aad964aa58fe6cd
山﨑陽聖 <yosei@yamazakiakirashounoMacBook-Air.local>
2021-03-10 23:18:07 +0900

    add Layout.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import React from "react"
-import Header from "../pages/components/header"
-import Footer from "../pages/components/footer"
 import Layout from "../pages/components/layout"
 
 const MainPage = () => {
   return (
     <div>
-    <Header/>
+    <Layout>
     <h1>Hello world!</h1>
-    <Footer/>
+    </Layout>
     </div>
   )
 }
