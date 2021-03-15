775b82af051d85fb30d7756b4495ed239c36775c
山﨑陽聖 <yosei@yamazakiakirashounoMacBook-Air.local>
2021-03-15 22:16:21 +0900

    add pillCase.png

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Header from './header'
+import Prof from "./profile"
 import Footer from './footer'
 import "./layout.css"
 
-const Layout = ( props ) => {
-    return (
+const Layout = (props) => {
+    return ( 
         <div>
-            <Header/>
-                { props.children }
-            <Footer/>
+        <Header/>
+        <Prof/>
+        <Footer/>
         </div>
     )
 }
---
+++ src/components/profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import pillCase from '../images/pillCase.png'
+
+const Prof = () => {
+    return (
+        <div className="pillCase"> 
+            <img src={pillCase} alt="pillCase"/>
+        </div> 
+    )
+}
+
+export default Prof
